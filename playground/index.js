98c37e80332ee905612d92107823e52b9815438f
Avi Goldman <avrahamymgoldman@gmail.com>
2021-07-08 08:59:54 -0400

    updated playground

--- playground/index.js
+++ playground/index.js
@@ -1,117 +1,105 @@
 import "regenerator-runtime/runtime";
 
-import Tape from "../packages/tape";
-import tapeSassPlugin from "../packages/plugin-sass";
-import tapeCSSInlinePlugin from "../packages/plugin-css-inline";
-import tapeHTMLMinifyPlugin from "../packages/plugin-html-minify";
-import tapeHTMLPrettifyPlugin from "../packages/plugin-html-prettify";
+import { tape } from "../packages/tape";
+import { sass } from "../packages/plugin-sass";
+import { cssInline } from "../packages/plugin-css-inline";
+import { htmlMinify } from "../packages/plugin-html-minify";
+import { htmlPrettify } from "../packages/plugin-html-prettify";
 
 const mimeType = {
   ".css": "text/css",
   ".html": "text/html",
 };
 
-const urlWrite = () => ({
-  name: "url-write",
-  write({ asset, cache }) {
-    if (asset.isEntry) {
-      return `${asset.source.path.replace(
-        new RegExp(`${asset.source.ext}$`),
-        asset.ext
-      )}`;
-    }
+const urlWrite = (config) => {
+  return {
+    name: "url-write",
+    write({ asset }) {
+      if (asset.isEntry) {
+        return `${asset.source.path.replace(
+          new RegExp(`${asset.source.ext}$`),
+          asset.ext
+        )}`;
+      }
 
-    const objectURL = URL.createObjectURL(
-      new Blob([asset.content], {
-        type: mimeType[asset.ext],
-      })
-    );
+      const objectURL = URL.createObjectURL(
+        new Blob([asset.content], {
+          type: mimeType[asset.ext],
+        })
+      );
 
-    cache.set(asset.id, objectURL);
-    return objectURL;
-  },
-  onChange({ asset, cache }) {
-    const objectURL = cache.get(asset.id);
+      config.cache.push(objectURL);
 
-    if (objectURL) {
-      cache.delete(asset.id);
-      URL.revokeObjectURL(objectURL);
-    }
-  },
-});
+      return objectURL;
+    },
+  };
+};
 
 const iframe = document.querySelector("iframe");
-const error = document.querySelector("#error");
-const baseFiles = {};
+const errorOverlay = document.querySelector("#error");
+let baseFiles = {};
 [...document.querySelectorAll(".editor")].forEach((editor) => {
   baseFiles[editor.getAttribute("data-file")] = {
     content: editor.querySelector("textarea").value,
   };
 });
-const tape = new Tape({
-  entry: "/index.html",
-  plugins: [
-    tapeSassPlugin,
-    tapeCSSInlinePlugin,
-    tapeHTMLMinifyPlugin,
-    // tapeHTMLPrettifyPlugin,
-    urlWrite,
-  ],
-  files: baseFiles,
-});
 
-// setTimeout(() => {
-//   tape.update({
-//     plugins: []
-//   })
-//   alert('changed plugins')
-// }, 5000)
+const cacheConfig = { cache: [] };
 
-const manager = tape.dev();
-manager.on("*", (event, data) => {
-  if (event === "end") {
+async function compile(update) {
+  baseFiles = {
+    ...baseFiles,
+    ...update,
+  };
+
+  for (let url of cacheConfig.cache) {
+    URL.revokeObjectURL(url);
+  }
+
+  try {
+    const { files, entry } = await tape({
+      entry: "/index.html",
+      plugins: [sass, cssInline, htmlMinify, [urlWrite, cacheConfig]],
+      files: baseFiles,
+    });
+
     iframe.contentWindow.document.open();
-    iframe.contentWindow.document.write(data.files[data.entry].content);
+    iframe.contentWindow.document.write(files[entry].content);
     iframe.contentWindow.document.close();
     error.innerHTML = "";
-  }
-
-  if (event === "error") {
-    console.log(data.error);
-    error.innerHTML = `
-      <html>
-      <body style="">
-        <div style="margin: 0; font-family: system-ui; color: #730f45;box-sizing: border-box;background: #fce4ec; padding: 20px; height: 250px; width: 100%; display: flex; justify-content: space-between; align-items: center;">
-          <div>
-          <h1 style=" font-size: 20px;">Error: ${data.error.message}</h1>
-          <pre style="width: 100%; white-space: break-spaces;">${
-            data.error.stack
-          }</pre>
-          ${
-            data.error.hasOwnProperty("path")
-              ? `
-            <code>File: ${data.error.path}</code><br/>
-            <code>Line: ${data.error.line}</code><br/>
-            <code>Column: ${data.error.column}</code><br/>
-          `
-              : ""
-          }
+  } catch (error) {
+    errorOverlay.innerHTML = `
+        <html>
+        <body style="">
+          <div style="margin: 0; font-family: system-ui; color: #730f45;box-sizing: border-box;background: #fce4ec; padding: 20px; height: 250px; width: 100%; display: flex; justify-content: space-between; align-items: center;">
+            <div>
+            <h1 style=" font-size: 20px;">Error: ${error.message}</h1>
+            <pre style="width: 100%; white-space: break-spaces;">${
+              error.stack
+            }</pre>
+            ${
+              error.hasOwnProperty("path")
+                ? `
+              <code>File: ${error.path}</code><br/>
+              <code>Line: ${error.line}</code><br/>
+              <code>Column: ${error.column}</code><br/>
+            `
+                : ""
+            }
+            </div>
           </div>
-        </div>
-      </body>
-      </html>
-    `;
+        </body>
+        </html>
+      `;
   }
-});
+}
 
 [...document.querySelectorAll("textarea")].forEach((textarea) => {
   textarea.addEventListener("input", () => {
     const file = textarea.parentElement.getAttribute("data-file");
-    tape.update({
-      files: {
-        [file]: {
-          content: textarea.value,
-        },
+    compile({
+      [file]: {
+        content: textarea.value,
       },
     });
   });
